4c83c37fde7b67c941436efe0e2ba91ea8ece062
Kishore <murekishore@gmail.com>
2022-03-10 09:13:34 +0530

    File reading in python

---
+++ main.py
@@ -0,0 +1,11 @@
+import csv
+import pandas as pd
+
+data = pd.read_csv('https://www1.nseindia.com/products/content/all_daily_reports.htm')
+print(data)
+with open("/Users/murek/Downloads/cm09MAR2022bhav.csv") as fp:
+        for line in csv.DictReader(fp):
+            #print(line)
+            if line['OPEN'] == line['HIGH']:
+              print(line['SYMBOL'])
+
